791c0edf4fb39ecd048e14ca5f37d44db7a9ab21
devdeep-arch <deepdeliver@gmail.com>
2026-02-06 20:34:52 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+process.on("uncaughtException", (err) => {
+  console.error("🔥 Uncaught Exception:", err);
+});
+
+process.on("unhandledRejection", (reason, promise) => {
+  console.error("🔥 Unhandled Rejection:", reason);
+});
 
 import express from "express";
 import crypto from "crypto";
@@ -1536,6 +1543,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`⚡ Server running on port ${PORT}`);
 });
+
 
 
 
